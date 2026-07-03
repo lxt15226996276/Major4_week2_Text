a522bed5753c41e31c61e85c981640ba97b6de5b
黎明之夜 <3027779731@qq.com>
2026-07-03 08:22:39 +0800

    编写Exam03 GameController
    首次编写游戏控制脚本，实现随机生成三个敌人

--- Assets/Exams/Exam03/Scripts/GameController.cs
+++ Assets/Exams/Exam03/Scripts/GameController.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+namespace Exam.Exam03
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Exam03 主场景：随机刷 3 个敌人。敌人总血量 UI 在第 3 步已显示 0，本脚本不改。
+    /// </summary>
+    public class GameController : MonoBehaviour
     {
-        
-    }
+        [Header("刷怪")]
+        [SerializeField] private GameObject enemyPrefab;
+        [SerializeField] private int enemyCount = 3;
+        [SerializeField] private float spawnRangeX = 22.5f;
+        [SerializeField] private float spawnRangeZ = 22.5f;
+        [SerializeField] private float spawnY = 0.35f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            SpawnEnemies();
+        }
+
+        /// <summary>
+        /// 思路：循环 3 次 Instantiate，X/Z Random，Y 略抬高。
+        /// </summary>
+        private void SpawnEnemies()
+        {
+            if (enemyPrefab == null) return;
+
+            for (int i = 0; i < enemyCount; i++)
+            {
+                float x = Random.Range(-spawnRangeX, spawnRangeX);
+                float z = Random.Range(-spawnRangeZ, spawnRangeZ);
+                Vector3 pos = new Vector3(x, spawnY, z);
+                Instantiate(enemyPrefab, pos, Quaternion.identity);
+            }
+        }
     }
 }
